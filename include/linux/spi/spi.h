376c9691e2df3ca5aa56f037a409a33c7517e267
Troy Kisky <troy.kisky@boundarydevices.com>
2017-06-27 16:31:39 -0700

    spi: add pinctrl/max_freq for read
    Signed-off-by: Troy Kisky <troy.kisky@boundarydevices.com>

--- include/linux/spi/spi.h
+++ include/linux/spi/spi.h
@@ -162,6 +162,7 @@ struct spi_device {
 	struct spi_controller	*controller;
 	struct spi_controller	*master;	/* compatibility layer */
 	u32			max_speed_hz;
+	u32			max_read_speed_hz;
 	u8			chip_select;
 	u8			bits_per_word;
 	bool			rt;
@@ -506,6 +507,7 @@ struct spi_controller {
 	/* limits on transfer speed */
 	u32			min_speed_hz;
 	u32			max_speed_hz;
+	u32			max_read_speed_hz;
 
 	/* other constraints relevant to this driver */
 	u16			flags;
@@ -516,6 +518,9 @@ struct spi_controller {
 #define SPI_CONTROLLER_MUST_TX		BIT(4)	/* requires tx */
 
 #define SPI_MASTER_GPIO_SS		BIT(5)	/* GPIO CS must select slave */
+
+	/* flag indicating this is a non-devres managed controller */
+	bool			devm_allocated;
 
 	/* flag indicating this is an SPI slave controller */
 	bool			slave;
@@ -673,6 +678,9 @@ struct spi_controller {
 
 	/* Interrupt enable state during PTP system timestamping */
 	unsigned long		irq_flags;
+	struct pinctrl		*pinctrl;
+	struct pinctrl_state	*pins_write;
+	struct pinctrl_state	*pins_read;
 
 	ANDROID_KABI_RESERVE(1);
 	ANDROID_KABI_RESERVE(2);
@@ -953,6 +961,7 @@ struct spi_transfer {
 #define	SPI_NBITS_SINGLE	0x01 /* 1bit transfer */
 #define	SPI_NBITS_DUAL		0x02 /* 2bits transfer */
 #define	SPI_NBITS_QUAD		0x04 /* 4bits transfer */
+	unsigned	read_setup:1;
 	u8		bits_per_word;
 	u16		delay_usecs;
 	struct spi_delay	delay;
